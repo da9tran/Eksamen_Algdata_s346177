f8d22eca85426b61d1ed0415aaa6770b300d0b11
Dani <dani2009no@gmail.com>
2020-10-20 14:00:23 +0200

    Oppgave 0: La til noen instanser av klassen EksamenSBinTre. Testet kodebiten; den blir kjørt og jeg får utskrift: 0.

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -1,5 +1,10 @@
 package no.oslomet.cs.algdat.Eksamen;
 
-public class Main {
+import java.util.Comparator;
 
+public class Main {
+    public static void main(String[] args) {
+        EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        System.out.println(tre.antall());
+    }
 }
