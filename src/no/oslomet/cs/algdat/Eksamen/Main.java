b141b90feb1f78bbec9113e1bfdc96c137516244
Dani <dani2009no@gmail.com>
2020-10-21 11:34:28 +0200

    Oppgave 1: Kopierte Programkode 5.2 3 a)

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
         System.out.println(tre.antall());
+
+        
     }
 }
