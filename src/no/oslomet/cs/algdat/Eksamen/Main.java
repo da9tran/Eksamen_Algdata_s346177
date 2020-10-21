62185855e55057631b2ef22eca4f5535e0a63b47
Dani <dani2009no@gmail.com>
2020-10-21 11:45:02 +0200

    Oppgave 1: Sjekket følgende programbit, som gir utskrift 10.

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -5,8 +5,12 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
         EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
-        System.out.println(tre.antall());
+        System.out.println(tre.antall()); //Utskrift: 0
 
-        
+        Integer[] a = {4, 7, 2, 9, 5, 10, 8, 1, 3, 6};
+        EksamenSBinTre<Integer> tre1 = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre1.leggInn(verdi);
+        System.out.println(tre1.antall()); //Utskrift: 10
+
     }
 }
