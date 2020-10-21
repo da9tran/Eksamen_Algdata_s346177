bdfb479a2e7ac6f1a4a5afdd035b00ae51776a2e
Dani <dani2009no@gmail.com>
2020-10-21 12:27:17 +0200

    Oppgave 2: Testet kodebiten på oppgave 2, ble kjørt og fikk utskrift: 10, 0, 3, 2, 1.

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -12,5 +12,14 @@ public class Main {
         for (int verdi : a) tre1.leggInn(verdi);
         System.out.println(tre1.antall()); //Utskrift: 10
 
+        Integer [] a1 = {4,7,2,9,4,10,8,7,4,6};
+        EksamenSBinTre<Integer> tre2 = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a1) tre2.leggInn(verdi);
+
+        System.out.println(tre2.antall()); //Utskrift: 10
+        System.out.println(tre2.antall(5)); //Utskrift: 0
+        System.out.println(tre2.antall(4)); //Utskrift: 3
+        System.out.println(tre2.antall(7)); //Utskrift: 2
+        System.out.println(tre2.antall(10)); //Utskrift: 1
     }
 }
