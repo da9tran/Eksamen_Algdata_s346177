c76990292b679f252d5adb45e36862b5d5b0fd80
Dani <dani2009no@gmail.com>
2020-10-23 22:55:46 +0200

    Oppgave 3: Lånte en kodebit fra kompendiet (kapittel 5), og implementerte metoden private static <T> Node<T> førstePostorden(Node<T> p).

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -137,7 +137,13 @@ public class EksamenSBinTre<T> {
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (p == null) throw new NoSuchElementException("Treet er tomt!");
+
+        while (true) {
+            if (p.venstre != null) p = p.venstre;
+            else if (p.høyre != null) p = p.høyre;
+            else return p;
+        }
     }
 
     private static <T> Node<T> nestePostorden(Node<T> p) {
